8efcd8fb53a844530ec1a61ede751d87173ec9e9
AnnikaWe <32836712+AnnikaWe@users.noreply.github.com>
2025-04-05 11:27:44 +0000

    doc creation config files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,25 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
 # -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'coding-challenge'
+copyright = '2025, Annika Weisser'
 author = 'Annika Weisser'
-release = '1.0'
+release = '1.0.0/00'
 
 # -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'sphinx.ext.napoleon',  
-    'sphinx.ext.viewcode',  
-]
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = []
 
+
+
 # -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster' 
+html_theme = 'alabaster'
 html_static_path = ['_static']
-
-# -- Additional options ------------------------------------------------------
-
-# language 
-# language = 'de'
